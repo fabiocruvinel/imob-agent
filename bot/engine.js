79b1c5628e3f764e002fd651e895959ceae8232e
fabiocruvinel <84475074+fabiocruvinel@users.noreply.github.com>
2026-03-03 22:48:04 -0300

    Atualizar o engine.js

--- bot/engine.js
+++ bot/engine.js
@@ -1,18 +1,34 @@
-export async function handleMessage(client, message) {
+export async function handleMessage(text){
 
-const text = message.text.body.toLowerCase()
+text = text.toLowerCase()
 
 if(text.includes("oi") || text.includes("olá")){
 
-return `
-Olá! 👋
+return `Olá 👋
 
-Sou o assistente da ${client.name}
+Sou o assistente virtual da imobiliária.
+
+Como posso ajudar?
 
 1️⃣ Comprar imóvel
 2️⃣ Vender imóvel
 3️⃣ Falar com corretor
 `
+
 }
 
+if(text === "1"){
+
+return `Ótimo!
+
+Que tipo de imóvel você procura?
+
+🏠 Casa
+🏢 Apartamento
+🌱 Lote
+`
+
+}
+
+return "Desculpe, não entendi. Digite *oi* para começar."
 }
