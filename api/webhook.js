1333d47aae5ec72adcfff7bdeb4e3a7c92c325c7
fabiocruvinel <84475074+fabiocruvinel@users.noreply.github.com>
2026-03-04 00:17:11 -0300

    Atualizar o webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -8,17 +8,27 @@ export default async function handler(req, res) {
 
     if (body.object) {
 
-      const message =
-        body.entry?.[0]?.changes?.[0]?.value?.messages?.[0]
+      const message = body.entry?.[0]?.changes?.[0]?.value?.messages?.[0]
 
       if (message) {
 
         const from = message.from
 
-        await sendWhatsAppMessage(
-          from,
-          "Olá 👋\n\nSou o assistente virtual da imobiliária.\n\n1️⃣ Comprar imóvel\n2️⃣ Vender imóvel\n3️⃣ Falar com corretor"
-        )
+        await fetch(`https://graph.facebook.com/v22.0/934741396399050/messages`, {
+          method: "POST",
+          headers: {
+            "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`,
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            messaging_product: "whatsapp",
+            to: from,
+            type: "text",
+            text: {
+              body: "Olá 👋\nSou o assistente virtual da imobiliária.\n\nDigite:\n1 - Comprar imóvel\n2 - Vender imóvel\n3 - Falar com corretor"
+            }
+          })
+        })
 
       }
 
