98c9648dd35accef91f69cc35a6e8f7b72fb9526
fabiocruvinel <84475074+fabiocruvinel@users.noreply.github.com>
2026-03-03 22:42:00 -0300

    Criar services/whatsapp.js

---
+++ services/whatsapp.js
@@ -0,0 +1,17 @@
+export async function sendMessage(phoneId, token, to, text){
+
+await fetch(`https://graph.facebook.com/v19.0/${phoneId}/messages`,{
+method:"POST",
+headers:{
+Authorization:`Bearer ${token}`,
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+messaging_product:"whatsapp",
+to,
+type:"text",
+text:{body:text}
+})
+})
+
+}
